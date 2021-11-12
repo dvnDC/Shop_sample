b842d6d84ec17f283930cc79faff5639ebb28f2f
Damian Cichosz <dcichosz@cross-net.eu>
2021-11-12 16:33:51 +0100

    Created Unit tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,28 @@
-require "test_helper"
+require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
+  def setup
+    @base_title = "Shop page"
   end
 
-  test "should get help" do
-    get static_pages_help_url
+  test "should get home" do
+    get root_path
     assert_response :success
+    assert_select "title", "#{@base_title}"
+  end
+  test "should get help" do
+    get help_path
+    assert_response :success
+    assert_select "title", "#{@base_title}"
+  end
+  test "should get about" do
+    get about_path
+    assert_response :success
+    assert_select "title", "#{@base_title}"
+  end
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "#{@base_title}"
   end
 end
-
