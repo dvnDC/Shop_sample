b1ecd54e15cb602bbbe29d8c72392f8f4f4bd352
dvnDC <damcichosz@gmail.com>
2021-12-07 03:08:44 +0100

    Rebased to support CSS Bundling and JS Bundling

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,4 +1,11 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+// Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
-import "controllers"
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import "./controllers"
+import * as bootstrap from "bootstrap"
+
+document.addEventListener("turbo:load", () => {
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
+})
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -64,7 +64,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-  #
-  # Allow connections to local server. For cloud IDE.
-  # config.hosts.clear
 end
