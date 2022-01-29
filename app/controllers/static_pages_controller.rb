a4f1c64a5c5639b4f711d466cc5093e74605dd72
dvnDC <damcichosz@gmail.com>
2022-01-29 03:14:03 +0100

    Change paginate value for feed_microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def home
     if logged_in?
       @micropost = current_user.microposts.build
-      @feed_microposts = current_user.feed.paginate(page: params[:page])
+      @feed_microposts = current_user.feed.paginate(page: params[:page], :per_page => 12)
     end
   end
 
